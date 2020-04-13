8964880a75d2205c75b8e75304cfebd22c01bbec
Obskurcyte <hadrien.jaubert99@gmail.com>
2020-04-13 14:23:09 +0200

    Ajout de l'erreur 400

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,16 @@ let premiers = require('../premiers');
 let router = express.Router();
 
 router.post('/resultat', function(req, res){
-    res.send({
-        squareRoot: Math.sqrt(req.body.nbr),
-        primes: premiers(req.body.nbr)
-})
-
+    if ((nbr.isInteger()) && (nbr > 0)){
+        res.send({
+            squareRoot: Math.sqrt(req.body.nbr),
+            primes: premiers(req.body.nbr)
+    })} else {
+        res.send('Error 400')
+        }
 });
+
+
 
 
 module.exports = router;
